7fcea83fcc30da1727cb9bb25dd95251bf04de0e
seungju425 <seungju425@gmail.com>
2024-11-18 16:44:28 +0900

    내 주변 은행 찾기

--- final-pjt-front/src/App.vue
+++ final-pjt-front/src/App.vue
@@ -1,21 +1,25 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-
-</script>
-
 <template>
   <header>
     <div>
       <h1>메인 화면입니다.</h1>
 
       <nav>
-        
+        <RouterLink :to="{ name:'home'}">메인</RouterLink> |
+        <RouterLink :to="{ name:'map' }">내 주변 은행 찾기</RouterLink> |
       </nav>
+
     </div>
   </header>
 
   <RouterView />
 </template>
+
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
 
 <style scoped>
 
--- final-pjt-front/src/router/index.js
+++ final-pjt-front/src/router/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import MapView from '@/views/MapView.vue'
+import HomeView from '@/views/HomeView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView,
-    // },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
     // {
     //   path: '/about',
     //   name: 'about',
@@ -16,6 +17,11 @@ const router = createRouter({
     //   // which is lazy-loaded when the route is visited.
     //   component: () => import('../views/AboutView.vue'),
     // },
+    {
+      path:'/map',
+      name:'map',
+      component: MapView,
+    }
   ],
 })
 
