7cf501b9e6f75df92c40453859370fe3e6f4f746
unknown <happypigs7.dev@gmail.com>
2024-11-18 14:48:29 +0900

    스켈레톤 코드 작성 완료

---
+++ final-pjt-front/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    <div>
+      <h1>메인 화면입니다.</h1>
+
+      <nav>
+        
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
