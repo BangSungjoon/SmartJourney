f97c62f7a28da8a52f7a43638221725b839655dd
seungju <seungju425@gmail.com>
2024-11-25 15:14:33 +0900

    portfolio

--- final-pjt-front/src/components/pages/questions/PortResults.vue
+++ final-pjt-front/src/components/pages/questions/PortResults.vue
@@ -28,7 +28,7 @@
       </div>
 
       <!-- 오른쪽 원형 배치 -->
-      <div class="right-content">
+      <!-- <div class="right-content">
         <div v-if="selectedCriteria === '위험도'" class="large-circle">
           위험도 기반 추천
         </div>
@@ -38,7 +38,11 @@
           <div class="small-circle">종목 2</div>
           <div class="small-circle">종목 3</div>
         </div>
+      </div> -->
+      <div>
+        <PortChart :data="resultData"/>
       </div>
+
     </div>
 
     <!-- 하단 공유하기 버튼 -->
@@ -52,11 +56,98 @@
 
 
 <script setup>
-import { ref } from 'vue';
+import PortChart from './PortChart.vue';
+import { ref, nextTick } from 'vue';
+import { useRoute } from 'vue-router';
+
+
+// question.vue에서 전달된 resultData 가져오기
+const route = useRoute()
+const resultData = route.params.resultData;
+console.log(`결과 데이터: ${resultData}`)
 
 const selectedCriteria = ref("위험도"); // 기본값
 const applyFilters = () => {
   console.log(`선택된 기준: ${selectedCriteria.value}`);
+  nextTick(() => { // DOM 업데이트가 완료된 후 차트를 그릴 수 있게 보장하는 메서드
+        const ctx = document.getElementById('riskChart');
+
+        new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                labels: ['저위험', '중저위험', '중위험', '중고위험', '고위험'],
+                datasets: [{
+                    label: '위험도에 따른 투자 비율 구성',
+                    data: [props.portfolio.low_ratio, props.portfolio.med_low_ratio, props.portfolio.med_ratio, props.portfolio.med_high_ratio, props.portfolio.high],
+                    backgroundColor: [
+                        '#4CAF50', // 저위험 (밝고 차분한 초록)
+                        '#FFD700', // 중저위험 (노란색)
+                        '#FF6347', // 중위험 (토마토 색, 약간 강렬한 빨강)
+                        '#FF4500', // 중고위험 (주황색)
+                        '#FF0000'  // 고위험 (강렬한 빨강)
+                    ],
+                    hoverOffset: 4
+                }]
+            },
+        });
+
+        const ctx1 = document.getElementById('saveInv');
+
+        new Chart(ctx1, {
+            type: 'doughnut',
+            data: {
+                labels: ['저축', '투자'],
+                datasets: [{
+                    label: '투자 저축 비율',
+                    data: [props.portfolio.saving_ratio*100, props.portfolio.inv_ratio*100],
+                    backgroundColor: [
+                        '#4CAF50', // (밝고 차분한 초록)
+                        '#FF0000'  // (강렬한 빨강)
+                    ],
+                    hoverOffset: 4
+                }]
+            },
+        });
+
+        const ctx2 = document.getElementById('InvChart');
+        new Chart(ctx2, {
+            type: 'doughnut',
+            data: {
+                labels: ['국내 주식형', '해외 주식형', '채권형', '대안투자'],
+                datasets: [{
+                    label: '자산 유형(투자 상품 내)에 따른 포트폴리오',
+                    data: [props.portfolio.dom_stock_ratio*100, props.portfolio.int_stock_ratio*100, props.portfolio.bond_ratio*100, props.portfolio.alt_invest_ratio*100],
+                    backgroundColor: [
+                        '#4CAF50', // 저위험 (밝고 차분한 초록)
+                        '#FFD700', // 중저위험 (노란색)
+                        '#FF4500', // 중고위험 (주황색)
+                        '#FF0000'  // 고위험 (강렬한 빨강)
+                    ],
+                    hoverOffset: 4
+                }]
+            },
+        });
+
+        const ctx3 = document.getElementById('saveChart');
+        new Chart(ctx3, {
+            type: 'doughnut',
+            data: {
+                labels: ['적금/정기예금', '보통예금'],
+                datasets: [{
+                    label: '자산 유형(저축 상품 내)에 따른 포트폴리오',
+                    data: [props.portfolio.inst_save_ratio*100, props.portfolio.reg_save_ratio*100],
+                    backgroundColor: [
+                        '#4CAF50', // 저위험 (밝고 차분한 초록)
+                        '#FF0000'  // 고위험 (강렬한 빨강)
+                    ],
+                    hoverOffset: 4
+                }]
+            },
+        });
+    })
+
+
+
 };
 </script>
 
